bb7d7e1ea0a330362c06099e73ab4cb614455439
Rhiannon Grant <rbgrant87@gmail.com>
2020-05-12 16:00:07 +1000

    Randomly distributed in pairs
    -The cards are randomly distributed in pairs, with different pics.
    -fixed a couple of the svgs.

--- build/app.js
+++ build/app.js
@@ -1,4 +1,5 @@
 const theCards = document.querySelectorAll(".the-card");
+const changeCardPic = document.querySelectorAll(".the-card use");
 const cardTypes = document.querySelectorAll("symbol");
 
 const cards = Array.from(theCards);
@@ -12,6 +13,80 @@ let picture1Location;
 let picture2Location;
 let getCard1;
 let getCard2;
+
+let numbers50 = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
+  49
+];
+
+let random20pairs = [];
+
+for (let i = 0; i < 20; i++) {
+  let randomNumber = Math.floor(Math.random() * numbers50.length);
+  random20pairs.push(numbers50[randomNumber], numbers50[randomNumber]);
+  numbers50.splice(randomNumber, 1);
+}
+
+let randomlyOrderCards = [];
+
+for (let j = 0; j < 40; j++) {
+  let randomNumber = Math.floor(Math.random() * random20pairs.length);
+  randomlyOrderCards.push(random20pairs[randomNumber]);
+  random20pairs.splice(randomNumber, 1);
+}
+
+for (let k = 0; k < 40; k++) {
+  let picture = "#" + cardTypes[randomlyOrderCards[k]].getAttribute("id");
+  changeCardPic[k].setAttribute("href", picture);
+}
 
 cards.forEach((card) => {
   card.addEventListener("click", function () {
@@ -41,3 +116,8 @@ cards.forEach((card) => {
     }
   });
 });
+
+console.log(random20pairs);
+console.log(numbers50);
+console.log(randomlyOrderCards);
+console.log(cards);
